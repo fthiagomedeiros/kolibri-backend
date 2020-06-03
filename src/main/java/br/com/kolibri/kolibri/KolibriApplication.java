509550a733727ebfdf4444f44df3055acace3e00
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2020-06-02 21:26:42 -0300

    first commit

---
+++ src/main/java/br/com/kolibri/kolibri/KolibriApplication.java
@@ -0,0 +1,13 @@
+package br.com.kolibri.kolibri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolibriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolibriApplication.class, args);
+    }
+
+}
